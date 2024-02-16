b4fb28b5f825c0cda8f5f0c88e1e9ed51db97b43
Gupta, Harshit Hemant <hhgupta@iu.edu>
2024-02-15 19:23:50 -0500

    changes done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const cors = require('cors')
-
+const express = require('express');
+const cors = require('cors');
+const {add}=require('./arthimetica');
 const app = express();
+
 app.use(cors()) // accross origin resourse sharing - basically we accept requests from all domains
 const port = 3000;
 
@@ -10,6 +11,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/add/:n/:m', (req, res) => {
+    //let n=Number(req.params.n);
+    //let m=Number(req.params.m);
     res.json(Number(req.params.n)+Number(req.params.m));
 });
 
